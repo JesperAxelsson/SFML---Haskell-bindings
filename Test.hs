2a5867399a4651e243469eee89a7addca28ca7fc
Sjur Gjøstein Karevoll <sjurberengal@gmail.com>
2011-04-06 17:35:01 +0200

    Window test

--- Test.hs
+++ Test.hs
@@ -1,10 +1,23 @@
 import SFML.System.Internal
-import Control.Concurrent
+import SFML.Window.Internal
 
 main = do
-  time <- clockGetTime c
-  c <- clockCreate
-  print time
-  threadDelay 1000000
-  time <- clockGetTime c
-  print time
+  w <- windowCreateSimple (VideoMode 800 600 32) "Hello world" [DefaultStyle]
+  go w
+ where
+   go w = do
+     exit <- processEvents w
+     if not exit
+       then do
+       windowDisplay w
+       go w
+       else
+       return ()
+   processEvents w = do
+     mEvt <- windowGetEvent w
+     case mEvt of
+       Nothing -> return False
+       Just (Closed) -> do
+         windowClose w
+         return True
+       Just _ -> return False
