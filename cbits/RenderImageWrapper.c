8b7c5d835bfae42ec28984638041b990cf305385
Sjur Gjøstein Karevoll <sjurberengal@gmail.com>
2011-04-07 22:48:21 +0200

    Added RenderImageWrapper

---
+++ cbits/RenderImageWrapper.c
@@ -0,0 +1,14 @@
+#ifndef SFML_RENDERIMAGE_WRAPPER
+#define SFML_RENDERIMAGE_WRAPPER
+
+CSFML_API void sfRenderImage_ClearWrapper(sfRenderImage* renderImage, sfColor *color)
+{
+  sfRenderImage_Clear(renderImage, *color);
+}
+
+CSFML_API void sfRenderImage_GetViewportWrapper(const sfRenderImage* renderImage, const sfView* view, sfIntRect *rect)
+{
+  *rect = sfRenderImage_GetViewport(renderImage, view);
+}
+
+#endif // SFML_RENDERIMAGE_WRAPPER
